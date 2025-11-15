1bade666b6e9e028691fcf84514b3cb63eab6dba
Debajit91 <debajitroy544@gmail.com>
2025-11-16 02:21:56 +0600

    explored key of operator

---
+++ src/keyOfConstraints.ts
@@ -0,0 +1,12 @@
+// keyof: type operator
+
+type RichPeopleVehicle = {
+    car: string;
+    bike: string;
+    cng: string;
+};
+
+type MyVehicle = 'bike' | 'car' | 'cng'
+type MyVehicle2 = keyof RichPeopleVehicle;
+
+const myVehicle:MyVehicle2 = 'cng';
