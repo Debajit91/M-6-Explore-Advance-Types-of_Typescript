0ec800464bd3932535dc0f9e623a1ad2f578cc8a
Debajit91 <debajitroy544@gmail.com>
2025-11-16 14:01:56 +0600

    key of constraints explored

--- src/keyOfConstraints.ts
+++ src/keyOfConstraints.ts
@@ -10,3 +10,41 @@ type MyVehicle = 'bike' | 'car' | 'cng'
 type MyVehicle2 = keyof RichPeopleVehicle;
 
 const myVehicle:MyVehicle2 = 'cng';
+
+// 
+type User = {
+    id: number;
+    name: string;
+    address:{
+        city: string
+    }
+}
+
+
+const user:User ={
+    id: 123,
+    name: 'Dev',
+    address:{
+        city: 'ctg'
+    }
+}
+
+const developer = {
+    id: 231,
+    name: 'Devs',
+    device:{
+        brand:'HP',
+        model: 'X-21'
+    }
+}
+
+const getPropertyFromObj = <T>(obj:T, key: keyof T) =>{
+    return obj[key];
+}
+
+const result = getPropertyFromObj(user, "address");
+console.log(result);
+
+const result1 = getPropertyFromObj(developer, 'device');
+
+console.log(result1);
